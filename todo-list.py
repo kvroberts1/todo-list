6d755c74238e45ef68555c61ea0a7a4a564a86b1
kvroberts1 <232481629+kvroberts1@users.noreply.github.com>
2026-03-04 17:54:17 -0500

    Starting Choice Tree with list

--- todo-list.py
+++ todo-list.py
@@ -1,4 +1,4 @@
-todo = []
+todo = ["Eat a tree"]
 status_off = 0
 
 while status_off == 0:
@@ -8,7 +8,12 @@ while status_off == 0:
     print("3. Add Item to List")
     print("4. Exit Application")
 
-    user_choice = input("Select a Choice: ")
+    user_choice = int(input("Select a Choice: "))
 
     if user_choice == 4:
         status_off = 1
+    else:
+        if user_choice == 1:
+            print(todo)
+        else:
+            print("Woo cowboy")
