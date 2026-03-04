ce5be65e1af289e1746b721ab52c41a987f272cd
kvroberts1 <232481629+kvroberts1@users.noreply.github.com>
2026-03-03 19:36:35 -0500

    Added text

--- todo-list.py
+++ todo-list.py
@@ -1,1 +1,14 @@
 todo = []
+status_off = 0
+
+while status_off == 0:
+    print("Welcome to your Todo List")
+    print("1. Review List")
+    print("2. Check Off Item")
+    print("3. Add Item to List")
+    print("4. Exit Application")
+
+    user_choice = input("Select a Choice: ")
+
+    if user_choice == 4:
+        status_off = 1
