4938f8747fd50880bd6c685cb9f07768235bab69
kvroberts1 <232481629+kvroberts1@users.noreply.github.com>
2026-03-05 17:18:19 -0500

    Added error handling

--- todo-list.py
+++ todo-list.py
@@ -13,18 +13,23 @@ while status_off == 0:
     print("3. Add Item to List")
     print("4. Exit Application")
 
-    user_choice = int(input("Select a Choice: "))
-
-    if user_choice == 4:
-        status_off = 1
-    else:
-        if user_choice == 1:
-            print_todo()
-        elif user_choice == 2:
-            delete_item = int(input("What Item would you like to delete: "))
-            todo.pop(delete_item)
-            print_todo()
-        elif user_choice == 3:
-            add_item = str(input("What would you like to add to your list: "))
-            todo.append(add_item)
-            print_todo()
+    try:
+        user_choice = int(input("Select a Choice: "))
+        if user_choice == 4:
+            status_off = 1
+        else:
+            if user_choice == 1:
+                print_todo()
+            elif user_choice == 2:
+                delete_item = int(input("What Item would you like to delete: "))
+                try:
+                    todo.pop(delete_item)
+                    print_todo()
+                except IndexError:
+                    print("Oops your list is empty or you are trying to delete and item that doesn't exsist.")
+            elif user_choice == 3:
+                add_item = input("What would you like to add to your list: ")
+                todo.append(add_item)
+                print_todo()
+    except ValueError:
+        print("Oops you can only select a number")
