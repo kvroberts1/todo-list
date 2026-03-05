359d680eae6b2a7640a1d9b7040b137397eb9898
kvroberts1 <232481629+kvroberts1@users.noreply.github.com>
2026-03-04 20:08:19 -0500

    Added function to print items on seperate lines.

--- todo-list.py
+++ todo-list.py
@@ -1,6 +1,11 @@
 todo = ["Eat a tree", "Listen to Weezer"]
 status_off = 0
 
+def print_todo():
+    for i in range(len(todo)):
+        print(i, todo[i])
+
+
 while status_off == 0:
     print("Welcome to your Todo List")
     print("1. Review List")
@@ -14,10 +19,12 @@ while status_off == 0:
         status_off = 1
     else:
         if user_choice == 1:
-            print(todo)
+            print(print_todo())
         elif user_choice == 2:
             delete_item = int(input("What Item would you like to delete: "))
             todo.pop(delete_item)
+            print_todo()
         elif user_choice == 3:
             add_item = str(input("What would you like to add to your list: "))
             todo.append(add_item)
+            print_todo()
